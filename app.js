8ed1213eb28edb6018669d784bfcce264f771de1
Esther <esther@pastel.codes>
2020-06-22 20:36:41 +0100

    Started development on about page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -13,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('common'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.disable('x-powered-by');
 
 app.use('/', indexRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

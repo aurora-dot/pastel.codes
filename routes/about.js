dd37e2e22c1d7d9a34990e33cbb74c83ca3c6dd3
Esther <esther@pastel.codes>
2020-06-27 17:46:21 +0100

    Possibly finished about page

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,28 @@
 var express = require('express');
+const axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('about', { title: 'About', description: "Who??? What??? AAAAaaa, about me." });
+    const ghost_key = process.env.GHOST_KEY
+    const base_url = `https://blog.pastel.codes/ghost/api/v3/content/posts/?key=${ghost_key}`
+
+    axios.all([
+        axios.get(`${base_url}&limit=3`),
+        axios.get(`${base_url}&limit=3&filter=tag:Project`),
+    ])
+        .then(axios.spread((response1, response2) => {
+            var base = { title: 'About', description: 'Who??? What??? AAAAaaa, about me.'};
+            var blog = JSON.parse(JSON.stringify(response1.data).split('"posts":').join('"blog":'));
+            var projects = JSON.parse(JSON.stringify(response2.data).split('"posts":').join('"project":'));
+            var out = Object.assign(base, blog, projects);
+
+            res.render('about', out);
+        }))
+        .catch(error => {
+            console.log(error);
+        });
+    // https://blog.pastel.codes/ghost/api/v3/content/posts/?key=
 });
 
 module.exports = router;

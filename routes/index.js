02f06ea8ed2780a361138d8d879330a16c712c4c
E <e@pastel.codes>
2021-06-24 18:08:23 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Home', description: "Hello, I'm Esther" });
+    res.render('index', { title: 'Home', description: "Hello, I'm E" });
 });
 
 module.exports = router;

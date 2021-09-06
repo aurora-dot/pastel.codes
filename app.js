a80a896029f2fbe53075bbce14a2f83e641226cf
E <e@pastel.codes>
2021-09-06 17:02:07 +0100

    Removed dead dependency

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var mLogger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
 var logger = require('./config/winston');
 const helmet = require("helmet");
 
@@ -45,12 +44,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

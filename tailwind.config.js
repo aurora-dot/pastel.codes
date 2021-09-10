e6cc660f5ec3e2820152a13e999ffdeb86b71508
E <e@pastel.codes>
2021-09-10 22:23:58 +0100

    Add tailwind fonts and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
   mode: "jit",
-  purge: ['../views/*.pug'],
+  purge: ['views/*.pug'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'extra': ['"Titling Gothic FB"'],
+        'sans': ['Gilroy'],
+        'mono': ['"Apercu Mono"'],
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: {
+          DEFAULT: '#002234',
+        },
+        pink: {
+          DEFAULT: '#CC7A98',
+        },
+        green: {
+          DEFAULT: '#CDE7B0',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
